763c1b830d4d8db6379d738e9315efa524e348fb
i-norden <iansnorden@gmail.com>
2022-03-14 10:09:26 -0500

    bump to use v1.10.16-statediff-3.0.2

--- src/go-ethereum-statediff/default.nix
+++ src/go-ethereum-statediff/default.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   name = "go-ethereum-statediff";
-  version = "v1.10.14-statediff-0.0.29";
+  version = "v1.10.16-statediff-3.0.2";
   src = fetchurl {
     url = "https://github.com/vulcanize/go-ethereum/releases/download/${version}/geth-linux-amd64";
-    sha256 = "0s1brrr7c36y0pap1f4vll07n2lvjc90yxa6rhx5g6jpsivmfci5";
+    sha256 = "0c488ns6w6jzcwwvw3p29i3n7yyp7ji8qy91133cwy4v37zl5lnk";
   };
 
   phases = ["installPhase" "patchPhase"];
